414bb4d3fac2b18139b08a0fb69f3bbe0b53fdb8
yifeishiYS <146201098+yifeishiYS@users.noreply.github.com>
2023-09-27 15:06:43 +0100

    Update frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,10 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for key in items:
+        key = str(key)
+        if key not in frequencies:
+            frequencies[key] = 1
+        else:
+            frequencies[key] += 1
     return frequencies
